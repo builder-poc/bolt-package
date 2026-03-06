e0ba39b63c992af06406c47aac1e830c8fec18bb
builder-poc <webbc33@nationwide.com>
2026-03-06 16:06:37 -0500

    7.0.3

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
-import "./bundle-core/bolt-core.mjs";
+/**
+ * Bolt package entrypoint for Builder usage.
+ *
+ * Recommended usage pattern:
+ *
+ * import * as Bolt from "@builder-poc/bolt-package/bundle-core";
+ * import "@builder-poc/bolt-package/css-core";
+ *
+ * Bolt.initialize();
+ *
+ * Do NOT import component paths such as:
+ * @builder-poc/bolt-package/textfield
+ */
+
+export * from "./bundle-core/bolt-core.mjs";
